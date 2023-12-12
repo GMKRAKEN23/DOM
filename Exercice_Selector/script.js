c1e92333587f03600dd3efa82a93b93d41802371
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-12-12 12:13:43 +0100

    Exercice selector finish

--- Exercice_Selector/script.js
+++ Exercice_Selector/script.js
@@ -1,8 +1,40 @@
-const classImportant = document.querySelectorAll(".important");
+/* const classImportant = document.querySelectorAll(".important");
 classImportant.setAttribute("title", "This is an important item");
 
 /* bad_pratique
 for(let i = 0; i <= 2 && i < classImportant.length; i++){
     classImportant[i].setAttribute("title", "This is an important item");
 }
+
+
+const image = document.querySelectorAll('img');
+
+image.forEach(img => {
+    if(!img.classList.contains('important')){
+        img.style.display = "none";
+    }
+});
+
+
+
+const para = document.querySelectorAll('p');
+
+para.forEach((paragraph) => {
+    console.log(paragraph.innerText);
+    console.log(paragraph.classList);
+});
 */
+
+const para = document.querySelectorAll('p');
+console.log(para);
+function addRandomColor(paragraph){
+    const red = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    console.log(paragraph.classList.contains('important'))
+    if (paragraph.classList.length > 0){
+        paragraph.style.color = `rgb(${red}, ${blue}, ${green})`;
+    }
+}
+
+para.forEach(addRandomColor);
