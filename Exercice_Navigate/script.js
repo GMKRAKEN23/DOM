5ccdcae627bd3ce464be3baebb65af7d738fcd3b
GMKRAKEN23 <[messinagiuseppe9423@gmail.com]>
2023-12-17 20:37:46 +0100

    add exercice_event

--- Exercice_Navigate/script.js
+++ Exercice_Navigate/script.js
@@ -1,2 +1,18 @@
-/*
-*/
+let ol = document.querySelector('ol');
+let lastchildren = ol.lastElementChild;
+
+ol.insertBefore(lastchildren, ol.firstElementChild);
+
+let main = document.querySelector('main');
+
+let section2 = document.querySelector('section:nth-child(2)');
+let section3 = document.querySelector('section:nth-child(3)');
+
+let h2Section2 = document.querySelector('section:nth-child(2) h2');
+let h2Section3 = document.querySelector('section:nth-child(3) div h2');
+
+
+
+let lastSection = document.querySelector('section:nth-child(3)');
+
+lastSection.parentElement.removeChild(lastSection); 
